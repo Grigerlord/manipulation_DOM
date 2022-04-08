934b24e817af19e009d99886bbd455d209f90eac
Grigerlord <Grigerlord@users.noreply.github.com>
2022-04-08 21:53:44 +0000

    deploy: 7946b91aa50d435073a4598a7e0005dd8ebb4e21

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,40 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo"
+
+//WEB API
+//Conectarnos al servidor
+    //TAREA: Usar async await
+window
+    .fetch(url)
+    //Procesar la respuesta y convertirla en JSON
+    .then((respuesta) => respuesta.json())
+    //JSON -> Data -> Renderizar info en el browser
+    .then((responseJson) => {
+        const todosLosItems = []
+        responseJson.data.forEach(item => {
+
+            //crear imagen
+            const image = document.createElement('img')
+
+            //crear titulo
+            const title = document.createElement('h2')
+
+            //crear precio
+            const price = document.createElement('div')
+
+
+            const container = document.createElement('div')
+            // container.appendChild(image)
+            // container.appendChild(title)
+            // container.appendChild(price)
+            container.append(image, title, price)
+
+            //document.body.appendChild(container)
+
+            todosLosItems.push(container)
+        })
+
+        document.body.append(...todosLosItems)
+    })
