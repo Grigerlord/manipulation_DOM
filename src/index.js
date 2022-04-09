7849d2696cc415fd908d634b3c5608e287069a10
Griger <grigerratia@gmail.com>
2022-04-09 13:31:27 -0400

    Clase #13: Enriqueciendo la Informacion

--- src/index.js
+++ src/index.js
@@ -3,28 +3,36 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = "https://platzi-avo.vercel.app/api/avo"
+
+const baseUrl = "https://platzi-avo.vercel.app"
+
+const appNode = document.querySelector('#app')
 
 //WEB API
 //Conectarnos al servidor
     //TAREA: Usar async await
 window
-    .fetch(url)
+    .fetch(`${baseUrl}/api/avo`)
     //Procesar la respuesta y convertirla en JSON
     .then((respuesta) => respuesta.json())
     //JSON -> Data -> Renderizar info en el browser
     .then((responseJson) => {
+
         const todosLosItems = []
+
         responseJson.data.forEach(item => {
 
             //crear imagen
             const image = document.createElement('img')
+            image.src = `${baseUrl}${item.image}`//URL de la imagen
 
             //crear titulo
             const title = document.createElement('h2')
+            title.textContent = item.name
 
             //crear precio
             const price = document.createElement('div')
+            price.textContent = item.price
 
 
             const container = document.createElement('div')
@@ -38,5 +46,33 @@ window
             todosLosItems.push(container)
         })
 
-        document.body.append(...todosLosItems)
+        appNode.append(...todosLosItems)
     })
+
+
+// const url = "https://platzi-avo.vercel.app/api/avo";
+
+// //web api
+// async function fetchData() {
+//   const response = await fetch(url),
+//   data = await response.json(),
+//   allItems = [];
+
+//   data.data.forEach((item) => {
+//     // create image
+//     const image = document.createElement("img");
+//     // create title
+//     const title = document.createElement("h2");
+//     // create price
+//     const price = document.createElement("div");
+
+//     const container = document.createElement("div");
+//     container.append(image, title, price);
+
+//     allItems.push(container);
+//   });
+
+//   document.body.append(...allItems)
+// }
+
+// fetchData();
